1532df53150a5f8e6c25f1880f4ef93dcd68007b
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-02-10 20:35:13 +0200

    add header

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -57,7 +57,7 @@ const Navbar = () => {
             <div className="gpt3__navbar-menu__container scale-up-center">
               <div className="gpt3__navbar-menu_container-links">
                 <Menu />
-                <div className="gpt3__navbar-menu_container-links-sign">
+                <div className="gpt3__navbar-menu__container-links-sign">
                   <p>Sign in</p>
                   <button className="btn" type="button">
                     Sign up
--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -2,7 +2,13 @@ import React from 'react'
 import "./header.css"
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt__headervsection_padding' id='home'>
+<div className='gpt__header-content'>
+  <h1 className='graident__text'>
+    
+  </h1>
+</div>
+    </div>
   )
 }
 
