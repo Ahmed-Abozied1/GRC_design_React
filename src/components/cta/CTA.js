e9e4a55fe3c003cb3058df6b852602a311b1e294
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-02-14 21:12:10 +0200

    add CTA component

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import "./cta.css"
+import React from "react";
+import "./cta.css";
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+  <div className="cta">
+      <div className="gpt3__cta">
+      <div className="gpt3__cta__content">
+        <p>Request Early Access to Get Started</p>
+        <h6>Register today & start exploring the endless possiblities.</h6>
+      </div>
+      <div className="gtp__cta__btn">
+        <button>Get Start</button>
+      </div>
+    </div>
+  </div>
+  );
+};
 
-export default CTA
+export default CTA;
--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -13,6 +13,7 @@ const Possibility = () => {
      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
      <h4>Request Early Access to Get Started</h4>
       </div>
+     
     </div>
   );
 };
