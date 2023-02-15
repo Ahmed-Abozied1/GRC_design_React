45801dbb20bc29741c2e5e3326ec8d87841ffa25
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-02-15 23:40:11 +0200

    add footer component

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import logo from "../../assets/GPT-3.svg";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="gpt3__footoer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links-logo">
+          <img src={logo} alt="GPT3" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Counters</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
