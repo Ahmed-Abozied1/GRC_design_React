9685357336f3bd8e4358fb0452e6b9a379112f31
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-02-15 22:16:10 +0200

    add Blog component

--- src/components/artical/Artical.js
+++ src/components/artical/Artical.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./artical.css"
-const Artical = () => {
+const Artical = ({imgUrl}) => {
   return (
-    <div>Artical</div>
+    <div className='gpt3__blog__container_artical'>
+
+
+<div className='gpt3__blog__container_artical_image'>
+  <img src={imgUrl} alt="BlogImage" />
+</div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.js
+++ src/containers/blog/Blog.js
@@ -1,9 +1,28 @@
-import React from 'react'
-import "./blog.css"
+import React from "react";
+import Artical from "../../components/artical/Artical";
+import "./blog.css";
+import {blog1,blog2,blog3,blog4,blog5} from "./imports"
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog__heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog__container">
+        <div className="gpt3__blog__container_groupA">
+          <Artical  imgUrl={blog1}/>
+        </div>
+        <div className="gpt3__blog__container_groupB">
+        <Artical/>
+        <Artical/>
+        <Artical/>
+        <Artical/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
